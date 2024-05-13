0e75291b9f522a4b18e4b2c25493a1f081211ef0
Azkalyasin <yasinadzkiaazkal@gmail.com>
2024-05-13 10:31:55 +0700

    pembuatan class dengan nama Mahasiswa

--- Praktimudasjar/Praktimudasjar.cpp
+++ Praktimudasjar/Praktimudasjar.cpp
@@ -3,6 +3,24 @@
 #include <string>
 using namespace std;
 
+class mahasiswa {
+public:
+    static int nim;
+    int id;
+    string nama;
+
+    void setID();
+    void PrintAll();
+
+    mahasiswa(string pnama) 
+    {
+        nama = pnama;
+        setID();
+    }
+
+};
+
+
 
 int main()
 {
